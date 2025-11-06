84038b9ea815126164efe6a8f2e66c35eeed31ed
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2025-11-06 16:39:51 +0300

    port değişikliği

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from routes import routes
 import os
-from flask_cors import CORS
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    os.makedirs("outputs", exist_ok=True)
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render'ın atadığı port
+    app.run(host="0.0.0.0", port=port)
